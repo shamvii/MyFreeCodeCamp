773c88e09838f476222883b3940e01dadcfaf350
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-02-06 21:25:25 -0500

    Multiple Identical Options in Switch Statements

---
+++ switch2.js
@@ -0,0 +1,36 @@
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+  }
+  
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+sequentialSizes(1);
+sequentialSizes(2);
+sequentialSizes(3);
+sequentialSizes(4);
+sequentialSizes(5);
+sequentialSizes(6);
+sequentialSizes(7);
+sequentialSizes(8);
+sequentialSizes(9);
+
