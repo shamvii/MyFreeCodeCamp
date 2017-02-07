d962683900e7a81d05b190375646a713edfa375c
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-02-06 20:46:00 -0500

    Concatenating Strings with the Plus Equals Operator

---
+++ concat_2.js
@@ -0,0 +1,11 @@
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr;
+myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
