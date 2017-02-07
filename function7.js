236081780d16c4b7b1314cfd9eac3040f552badf
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-02-06 21:08:47 -0500

    Assignment with a Returned Value

---
+++ function7.js
@@ -0,0 +1,22 @@
+
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+var processed = 0;
+processArg(7);
+processed = processArg(7);
+
