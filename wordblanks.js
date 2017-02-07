e4e6871c33db9e6fbda974b52a2addd2e5f6e6c6
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-02-06 20:54:22 -0500

    Word Blanks

---
+++ wordblanks.js
@@ -0,0 +1,11 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+  result = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("cat", "little", "hit", "slowly");
