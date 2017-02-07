33a1b12c99a844601262c35676933a843212d6b8
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-02-06 21:16:35 -0500

    Comparison with the Less Than Operator

---
+++ operators3.js
@@ -0,0 +1,20 @@
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+  
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+testLessThan(10);
+testLessThan(24);
+testLessThan(25);
+testLessThan(54);
+testLessThan(55);
+testLessThan(99);
