f239dc04c640e42333abd998dc75c0f6ae49a2a8
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-02-06 21:56:00 -0500

    Make Unique Objects by Passing Parameters to our Constructor

---
+++ const3.js
@@ -0,0 +1,10 @@
+
+var Car = function(wheels, seats, engines) {
+  //Change this constructor
+  this.wheels = wheels;
+  this.seats = seats;
+  this.engines = engines;
+};
+var myCar = new Car(8, 5, 2);
+//Try it out here
+var myCar;
