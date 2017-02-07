0d89bd0cafa1a264995ba4c935d5c05c5c92c16c
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-02-06 21:36:33 -0500

    Manipulating Complex Objects

---
+++ objects10.js
@@ -0,0 +1,23 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  {
+  "artist":"John Mayer",
+  "title": "Singer",
+  "release_year": 1988,
+  "formats": [
+  "CS",
+  "CD" ],
+    "gold": true
+
+  }
+];
+
