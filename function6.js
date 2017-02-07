5b20b3eb7ed61022265042d91d593aac9e5bbc63
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-02-06 21:07:47 -0500

    Return a Value from a Function with Return

---
+++ function6.js
@@ -0,0 +1,13 @@
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(n) {
+  return n*5;
+}
+timesFive(5);
+timesFive(2);
+timesFive(0);
