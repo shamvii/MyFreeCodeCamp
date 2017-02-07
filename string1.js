eea1cb8a7f5f93c4f144de7af5a435b3b7cbada3
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-02-06 20:47:16 -0500

    Constructing Strings with Variables

---
+++ string1.js
@@ -0,0 +1,13 @@
+// Example
+var ourName = "Free Code Camp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName;
+var myStr;
+myName = "Shambhavi";
+myStr = "My name is" + myName + " and I am swell!";
+
+
+
+
